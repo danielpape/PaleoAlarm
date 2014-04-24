030ca4f6e288e466a899aa98d4642938e4b4e7c4
Daniel Pape <dan@daniels-mbp.default>
2014-04-24 19:58:41 +0100

    Initial Commit

---
+++ paleoalarm/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  paleoalarm
+//
+//  Created by Daniel Pape on 24/04/2014.
+//  Copyright (c) 2014 Daniel Pape. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ paleoalarm/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  paleoalarm
+//
+//  Created by Daniel Pape on 24/04/2014.
+//  Copyright (c) 2014 Daniel Pape. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
